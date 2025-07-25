801c5b202590e8268f8b8c1cb6a955ca60abd0b5
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2025-07-25 18:21:21 +0300

    add them and mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import RoomCanvas from "@/components/features/room/RoomCanvas/RoomCanvas";
 import styles from "./page.module.css";
+import Button from '@mui/material/Button';
+import { Typography } from "@mui/material";
 
 const Home = () => {
   return (
     <main>
       <h1>Hello78978978789789798798</h1>
+      <Typography variant="h2" component="h1" color="primary">Hello</Typography>
+      <Button variant="contained" color="secondary">Test</Button>
       <RoomCanvas />
     </main>
   );
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,18 +3,46 @@ import { ReactNode } from "react";
 import { persistor, store } from "@/lib/redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const Providers = ({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#fafbfe',
+      },
+      secondary: {
+        main: '#12a4d5',
+        contrastText: '#fafbfe'
+      },
+      background: {
+        default: '#172430',
+      },
+    },
+    typography: {
+      fontFamily: 'var(--font-roboto-mono)',
+      h1: { fontFamily: 'var(--font-jetbrains-mono)' },
+      h2: { fontFamily: 'var(--font-jetbrains-mono)' },
+      h3: { fontFamily: 'var(--font-jetbrains-mono)' },
+      h4: { fontFamily: 'var(--font-jetbrains-mono)' },
+      h5: { fontFamily: 'var(--font-jetbrains-mono)' },
+      h6: { fontFamily: 'var(--font-jetbrains-mono)' },
+      fontSize: 14,
+    },
+    spacing: 8,
+  });
   return (
-    <Provider store={store}>
-      {/* <PersistGate persistor={persistor} loading={null}> */}
-        {children}
-      {/* </PersistGate> */}
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        {/* <PersistGate persistor={persistor} loading={null}> */}
+          {children}
+        {/* </PersistGate> */}
+      </Provider>
+    </ThemeProvider>
   );
 };
 
