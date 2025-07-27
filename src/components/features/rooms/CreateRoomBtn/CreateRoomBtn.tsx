d00148e33216b50e307ff08440f1dfb426643012
m1xture <misha.ivanov.1050@gmail.com>
2025-07-27 14:08:04 +0300

    hero-fix

--- src/components/features/rooms/CreateRoomBtn/CreateRoomBtn.tsx
+++ src/components/features/rooms/CreateRoomBtn/CreateRoomBtn.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 
 const StyledBox = styled.div`
   display: flex;
-  justify-content: center;
+justify-content: center;
 `;
 
 const CreateRoomBtn = ({ openCb }: { openCb: () => void }) => {
--- src/components/widgets/Hero/Hero.tsx
+++ src/components/widgets/Hero/Hero.tsx
@@ -1,7 +1,8 @@
 import { Button, Typography } from "@mui/material";
 import Container from "@/components/shared/layout/Container/Container";
 import Image from "next/image";
-import sofaImg from "../../../../public/hero/hero-sofa.png";
+import sofaImg from "../../../../public/hero/s2.png";
+import { FaArrowRightLong } from "react-icons/fa6";
 import css from "./Hero.module.scss";
 import Link from "next/link";
 
@@ -16,6 +17,8 @@ const Hero = () => {
               component="h1"
               color="primary"
               fontSize="2.5em"
+              marginBottom={"0.8em"}
+              textAlign={"left"}
             >
               Створіть власну кімнату мрії
             </Typography>
@@ -24,16 +27,26 @@ const Hero = () => {
               color="secondary"
               fontSize="1em"
               marginTop="0.8em"
-              marginBottom="1.4em"
+              marginBottom="2em"
               maxWidth="600px"
+              textAlign={"left"}
             >
               Ти уявляєш, а ми допомагаємо створити. З нашим редактором ти можеш
               легко перетягувати меблі, комбінувати стилі, змінювати кольори
               стін і бачити, як усе оживає прямо на екрані
             </Typography>
-            <Button variant="contained" LinkComponent={Link} href="/rooms">
-              Створити
-            </Button>
+            <div className={css.heroBlock}>
+              <Button
+                variant="contained"
+                LinkComponent={Link}
+                href="/rooms"
+                classes={css.heroBtn}
+                endIcon={<FaArrowRightLong />}
+                size="large"
+              >
+                Створити
+              </Button>
+            </div>
           </div>
           <Image src={sofaImg} alt="sofa" className={css.heroImg} />
         </div>
