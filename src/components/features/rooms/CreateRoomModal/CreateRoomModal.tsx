e6ce9aa2cbe7b53c55db413db8151ab43dc95751
m1xture <misha.ivanov.1050@gmail.com>
2025-07-27 23:55:40 +0300

    validation+unsavedchanges modal

--- src/components/features/rooms/CreateRoomModal/CreateRoomModal.tsx
+++ src/components/features/rooms/CreateRoomModal/CreateRoomModal.tsx
@@ -10,9 +10,22 @@ import {
   Button,
 } from "@mui/material";
 import { createRoom, selectAllRooms } from "@/lib/redux/rooms/roomsSlice";
-import { FormEvent, useMemo } from "react";
+import { FormEvent, useMemo, useState } from "react";
 import { AppDispatch } from "@/lib/redux/store";
 import styled from "styled-components";
+import z from "zod";
+
+type ErrorProps =
+  | {
+      error: true;
+      helperText: string;
+    }
+  | {};
+
+const roomNameSchema = z
+  .string({ error: "Недійсна назва кімнати" })
+  .min(3, { error: "Надто коротка назва" })
+  .max(35, { error: "Надто довга назва" });
 
 const StyledDialog = styled(Dialog)`
   & .MuiDialog-paperWidthSm {
@@ -34,6 +47,7 @@ const CreateRoomModal = ({
   closeCb: () => void;
 }) => {
   const allRooms = useSelector(selectAllRooms);
+  const [error, setError] = useState<ErrorProps>({});
   const dispatch: AppDispatch = useDispatch();
   const nextRoomNumber: number = useMemo(() => {
     return allRooms.length + 1;
@@ -45,7 +59,18 @@ const CreateRoomModal = ({
       "name"
     ) as HTMLInputElement;
     if (input) {
-      dispatch(createRoom(input.value.trim()));
+      const newRoomName = input.value.trim();
+      const validationResult = roomNameSchema.safeParse(newRoomName);
+      if (!validationResult.success) {
+        const errObj: ErrorProps = {
+          error: true,
+          helperText: validationResult.error.issues[0]?.message,
+        };
+        setError(errObj);
+        return;
+      }
+      setError({});
+      dispatch(createRoom(newRoomName));
       closeCb();
     }
   };
@@ -58,7 +83,6 @@ const CreateRoomModal = ({
         <form onSubmit={handleSubmit}>
           <StyledInput
             autoFocus={true}
-            required
             margin="dense"
             id="name"
             name="name"
@@ -67,6 +91,7 @@ const CreateRoomModal = ({
             placeholder={`Моя кімната #${nextRoomNumber}`}
             fullWidth
             variant="standard"
+            {...error}
           />
           <DialogActions>
             <Button onClick={closeCb}>Назад</Button>
